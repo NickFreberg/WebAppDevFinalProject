a7f8a8df8ba7131fd5ab0cc0ded4144991c59e34
Henri <48842995+HenriGit@users.noreply.github.com>
2019-12-17 05:22:59 -0500

    Add files via upload

--- routes/exam.js
+++ routes/exam.js
@@ -12,8 +12,6 @@ var passport = require('passport');
 var bcrypt = require('bcryptjs');
 
 
-
-
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.render('trythis', { user: req.user});
@@ -41,6 +39,7 @@ router.post('/', function(req, res, next) {
         client.query('INSERT INTO gameusers (username, password, value) VALUES($1, $2,1)', [req.body.username, newpass], function(err, result) {
         res.render('trythis', {successPass: "true"});
         console.log("we made it");
+        return res.redirect('/exam/quizone');
         });
       }
       else{
@@ -51,5 +50,45 @@ router.post('/', function(req, res, next) {
   });
 
 });
-  
+
+router.get('/quizone', function(req, res){
+  res.render('quizone',{user: req.user});
+});
+
+
+
+router.post('/quizone', function(req, res,next){
+  console.log("here");
+  client.query('SELECT * FROM gameusers WHERE username = $1', [req.user.username], function(err, result) {
+    console.log("it");
+    if (err) {
+      console.log("SQL error");
+      //next(err);
+    }
+    console.log("is");
+    if (result.rows.length > 0) {
+      console.log("user exist");
+      client.query('UPDATE gameusers SET value = $1 WHERE username = $2', [req.body.final, req.user.username], function(err, result) {
+        if (err) {
+          console.log("unable to query INSERT");
+          next(err);
+        }
+        console.log("updated values");
+        res.render('quizone', {user: req.user , success: "true" });
+      });
+    }
+    else{console.log("error checking is needed");}
+
+  });
+});
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
